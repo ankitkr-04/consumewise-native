faf8ccd524915ed6eff959b377e20f10140ee5a0
ankitkr-04 <ak0182274@gmail.com>
2024-09-17 05:38:02 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "expo",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "tailwindcss", "react-native"],
+  rules: {
+    "prettier/prettier": "warn",
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/enforces-shorthand": "warn",
+    "tailwindcss/migration-from-tailwind-2": "warn",
+    "tailwindcss/no-arbitrary-value": "warn",
+    "tailwindcss/no-custom-classname": "off",
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className="flex h-screen items-center justify-center">
+      <Text className="text-lg font-semibold text-red-400">Hello</Text>
+    </View>
+  );
+};
+export default index;
